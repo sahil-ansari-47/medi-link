ac8c3dd6bc48e0f64582530ebc24fe43d28b6502
sahil-ansari-47 <sahilansari28112003@gmail.com>
2025-07-07 01:29:03 +0530

    Finalize staticfiles + collectstatic config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 5173,
   },
   build: {
-    outDir: '../backend/staticfiles', // adjust if needed
+    outDir: '../medilink/staticfiles', // adjust if needed
     emptyOutDir: true,
   },
 })
--- medilink/settings.py
+++ medilink/settings.py
@@ -84,6 +84,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'medilink.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -139,6 +140,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_FILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL  = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
